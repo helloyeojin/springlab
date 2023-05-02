dbb274283462b3f0aa886c945c7df9ed42488e23
helloyeojin <bonuibomb@gmail.com>
2023-05-02 17:26:20 +0900

    0502 class

---
+++ src/main/java/com/kbstar/controller/ItemController.java
@@ -0,0 +1,80 @@
+package com.kbstar.controller;
+
+
+import com.github.pagehelper.PageInfo;
+import com.kbstar.dto.Cust;
+import com.kbstar.dto.Item;
+import com.kbstar.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+@Slf4j
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+
+    //Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    String dir = "item/";
+    @Autowired
+    ItemService service;
+
+    //127.0.0.1/item
+    @RequestMapping("")
+    public String main(Model model) {
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center", dir+"center");
+        return "index";
+    }
+
+    @RequestMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center", dir+"add");
+        return "index";
+    }
+
+    @RequestMapping("/all")
+    public String all(Model model) throws Exception {
+        List<Item> list = null;
+        try {
+            list = service.get();
+        } catch (Exception e) {
+            throw new Exception("시스템 장애:ER0002");
+        }
+//        List<Item> list = new ArrayList<>();
+//        list.add(new Item(100, "rose1", 1000, "a.jpg", new Date()));
+//        list.add(new Item(101, "rose2", 2000, "b.jpg", new Date()));
+//        list.add(new Item(102, "rose3", 3000, "c.jpg", new Date()));
+//        list.add(new Item(103, "rose4", 4000, "d.jpg", new Date()));
+//        list.add(new Item(104, "rose5", 5000, "e.jpg", new Date()));
+        model.addAttribute("allitem", list);
+
+        model.addAttribute("left", dir + "left");
+        model.addAttribute("center", dir + "all");
+        return "index";
+    }
+
+    @RequestMapping("/allpage")
+    public String allpage(@RequestParam(required = false, defaultValue = "1") int pageNo, Model model) throws Exception {
+        PageInfo<Item> p;
+        try {
+            p = new PageInfo<>(service.getPage(pageNo), 5); // 5:하단 네비게이션 개수
+        } catch (Exception e) {
+            throw new Exception("시스템 장애: ER0002");
+        }
+        model.addAttribute("target", "item");
+        model.addAttribute("cpage",p);
+        model.addAttribute("left",dir+"left");
+        model.addAttribute("center",dir+"allpage");
+        return "index";
+    }
+}
