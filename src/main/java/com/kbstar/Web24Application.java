5e80178b4f3c77b1bd42aa3d8d6eb70a6e51b758
helloyeojin <bonuibomb@gmail.com>
2023-05-23 17:42:13 +0900

    0523 git

---
+++ src/main/java/com/kbstar/Web24Application.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web24Application.class, args);
+    }
+
+}
